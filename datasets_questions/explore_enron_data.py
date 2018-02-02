cbb8bd736d90b5b78abf893d648ee21165f380e4
Dustin <magicdustin14@gmail.com>
2018-02-01 21:11:55 -0800

    Played around withg Enrons data set

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,5 +18,24 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+numberOfPOIs = 0
+for key in enron_data:
+  numberOfPOIs += enron_data[key]['poi']
 
+numberOfSalaries = 0
+for key in enron_data:
+  if (enron_data[key]['salary'] != 'NaN'):
+    numberOfSalaries += 1
+
+numberOfEmails = 0
+for key in enron_data:
+  if (enron_data[key]['email_address'] != 'NaN'):
+    numberOfEmails += 1
+
+print "Total persons of interest: ", numberOfPOIs
+print "Total salaries listed: ", numberOfSalaries
+print "Total emails listed: ", numberOfEmails
+print "Total pay to CEO: ", enron_data["SKILLING JEFFREY K"]["total_payments"] ,'$'
+print "Total pay to Chairman: ", enron_data["LAY KENNETH L"]["total_payments"] ,'$'
+print "Total pay to CFO: ", enron_data["FASTOW ANDREW S"]["total_payments"] ,'$'
 
